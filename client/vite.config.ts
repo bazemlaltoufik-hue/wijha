a41bdcf7ff4ced952079734daffc184262b4c9a3
toufikbazem <toufikbazem@gmail.com>
2026-01-23 20:24:35 +0100

    Changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  esbuild: {
+    logOverride: { "this-is-undefined-in-esm": "silent" },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
