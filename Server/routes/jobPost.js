cfd4816b39760f2049a156d5223bcbfcf0d06536
toufikbazem <toufikbazem@gmail.com>
2026-02-04 22:17:39 +0100

    Changes

--- Server/routes/jobPost.js
+++ Server/routes/jobPost.js
@@ -9,7 +9,7 @@ import {
 const router = express.Router();
 import { verifyUser } from "../utils/verifyUser.js";
 
-router.post("/create", verifyUser, createJobPost);
+router.post("/create", createJobPost);
 router.get("/getAllJobs", getAllJobPosts);
 router.post("/getJobPostsByIds", getJobPostsByIds);
 router.get("/:id", getJobPost);
